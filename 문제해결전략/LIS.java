b8c616448b105c1770abc2dd2e89ca4a464fbad6
현정 조 <cutececil@MacAir.local>
2016-08-19 13:15:24 +0900

    LIS Longest Increasing Subsequence

--- 문제해결전략/LIS.java
+++ 문제해결전략/LIS.java
@@ -1,5 +1,3 @@
-import java.io.File;
-import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -21,27 +19,48 @@ public class LIS {
 
             // input
             N = Integer.parseInt(sc.next()); // 1이상  ~ 100,000이하
-//            int A[] = new int[N];
             for (int i = 0; i < N; i++) {
                 S[i] = Integer.parseInt(sc.next());
             }
 
-            int res = lis(0);
+            // lis2 각 시작자리부터 끝까지 모든 숫자를 확인.
+            // 1 6 2 7 8 9 3 4 5의 경우,
+            // {1 2 7 8 9}, {1 2 3 4 5}, --- 0번째 (1)부터 시작한 모든 경우를 검사함
+            // {6 7 8 9}, {2 7 8 9} ...
+            int res = 0;
+            for (int i = 0; i < N; i++) {
+                res = Math.max(res, lis2(i));
+            }
+
+            // lis3 은 S[-1]부터 시작. 입력받은 수열 앞에 가상의 -1을 추가함 : 마지막 반환 결과에 -1을 빼면 개수는 동일
+            // {-1 1 2 7 8 9}, { -1 1 2 3 4 5} --- 등 한번에 다 검사함
+            //  int res = lis3(-1) - 1;
             System.out.println(res);
         }
 
         sc.close();
     }
 
+    static int lis2(int start) {
+        if (cache[start] != -1) return cache[start];
 
-    static int lis(int start) {
+        cache[start] = 1;
+        for (int next = start + 1; next < N; next++) {
+            if (S[start] < S[next])
+                cache[start] = Math.max(cache[start], lis2(next) + 1);
+        }
+
+        return cache[start];
+    }
+
+    static int lis3(int start) {
         int ret = cache[start + 1];
         if (ret != -1) return ret;
 
         cache[start + 1] = 1; // S[start] 는 항상 값이 있으므로 길이 최하는 1
         for (int next = start + 1; next < N; next++) {
             if (start == -1 || S[start] < S[next])
-                cache[start + 1] = Math.max(cache[start + 1], lis(next) + 1);
+                cache[start + 1] = Math.max(cache[start + 1], lis3(next) + 1);
         }
         return cache[start + 1];
     }
