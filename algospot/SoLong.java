5c02cc0f1cd455c6dcaf8804b412df0098fe0074
cutececil <cecilia.cho@lge.com>
2017-01-31 00:25:35 +0900

    Trie - wrong answer

--- algospot/SoLong.java
+++ algospot/SoLong.java
@@ -1,7 +1,3 @@
-import com.sun.xml.internal.xsom.impl.scd.Iterators;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -13,18 +9,9 @@ import java.util.*;
  */
 public class SoLong {
 
-    static class Dictionary {
-        int frequency;
-        String letter;
-
-        Dictionary(String l, int f) {
-            frequency = f;
-            letter = l;
-        }
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        //PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
@@ -32,61 +19,143 @@ public class SoLong {
             int N = Integer.parseInt(NM[0]);
             int M = Integer.parseInt(NM[1]);
 
-            ArrayList<Dictionary> dictList = new ArrayList<>(N);
+            ArrayList<Word> wordList = new ArrayList<>(N);
 
             for (int i = 0; i < N; i++) {
                 String[] dic = br.readLine().split(" ");
-                dictList.add(new Dictionary(dic[0], Integer.parseInt(dic[1])));
+                wordList.add(new Word(dic[0], Integer.parseInt(dic[1])));
             }
 
-            dictList.sort((Dictionary a, Dictionary b) -> {
-                if (a.frequency == b.frequency) {
-                    return a.letter.compareTo(b.letter);
+            // sort by count(frequency), and by word
+            wordList.sort((Word a, Word b) -> {
+                if (a.count == b.count) {
+                    return a.word.compareTo(b.word);
                 }
-                return b.frequency - a.frequency;
+                return b.count - a.count;
             });
+            //System.out.println(wordList.toString());
 
+            int index = 0;
             Trie trie = new Trie();
-            for (Dictionary w : dictList)
-                trie.insert(w.letter);
+            for (Word w : wordList) {
+                trie.insert(w.word, index++);
+            }
 
             String[] input = br.readLine().split(" ");
             int typings = 0;
             for (int i = 0; i < M; i++) {
-                String word = input[i];
-                System.out.println("word " + word);
+                //System.out.println("input " + input[i]);
 
-                int cnt = 0;
-                while (word.length() > cnt) {
-                    typings++;
-                    String test = word.substring(0, ++cnt);
-                    System.out.println("test " + test + " " + typings);
-                    TrieNode foundNode = trie.searchNode(test);
-                   // boolean found = trie.startsWith(test);
-
-                    if (test.equals(word)==false && foundNode!=null && foundNode.children.size() == 1/* && word.equals(foundNode.c)*/) {
-                        typings++; // tab key
-                        System.out.println("tab " + test + " " + typings);
-                        break;
-                    }
-                }
+                int cnt = countTypings(input[i], trie, wordList);
+                typings += cnt;
+                //System.out.println("input " + cnt);
             }
-            System.out.println(typings + M - 1);
+            System.out.println(typings + M - 1); // space count: M - 1
         }
 
         br.close();
-        out.close();
+       //out.close();
+    }
+
+    static int countTypings(String input, Trie trie, ArrayList<Word> wordList) {
+        TrieNode node = trie.search(input); // input 단어를 trie에서 가지고 있나
+
+        if (node == null || node.isLeaf == false) // not found
+            return input.length();
+
+        String word = wordList.get(node.leafId).word;
+        int wordLen = word.length();
+        int inputLen = input.length();
+        if (inputLen != wordLen)
+            return input.length();
+
+        int typings = trie.type(input, node.leafId);
+
+        return typings;
+    }
+
+    static class Word {
+        int count;
+        String word;
+
+        Word(String word, int count) {
+            this.count = count;
+            this.word = word;
+        }
+        public String toString() {
+            return word + "(" + count + ")";
+        }
     }
 
     static class TrieNode {
         char c;
-        HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+        TrieNode[] children;
         boolean isLeaf;
+        boolean isFirst;
+        int leafId;
 
-        public TrieNode() {}
+        public TrieNode() {
+        }
 
-        public TrieNode(char c){
+        public TrieNode(char c) {
             this.c = c;
+        }
+
+        public String toString() {
+            return this.c + "(" + leafId + ")";
+        }
+
+        void insert(char[] ch, int pos, int wordIndex) {
+            if (ch.length == pos) return;
+
+            int index = ch[pos] - 'A'; // toNumber
+            if (children == null) {
+                children = new TrieNode[26];
+                //children[index] = new TrieNode(ch[pos]);
+                //children[index].isFirst = true;
+                //children[index].leafId = wordIndex;
+            }
+
+            if (children[index] == null) {
+                children[index] = new TrieNode(ch[pos]);
+                children[index].isFirst = true;
+                children[index].leafId = wordIndex;
+            }
+
+            if (ch.length - 1 == pos) {
+                children[index].isLeaf = true;
+                children[index].leafId = wordIndex;
+            } else {
+                children[index].insert(ch, pos + 1, wordIndex);
+            }
+        }
+
+        TrieNode search(char[] ch, int pos) {
+            // ???
+            if (isLeaf == false && ch.length <= pos) return null;
+
+            //if (isLeaf) {
+            if (isLeaf || ch.length == pos) {
+                return this;
+            }
+
+            int index = ch[pos] - 'A';
+            if (children[index] == null) // no more children
+                return null;
+
+            return children[index].search(ch, pos + 1);
+        }
+
+        int type(char[] ch, int pos, int wordIndex) {
+            if (ch.length == pos)
+                return 0; // last character
+
+            if (isFirst && leafId == wordIndex)
+                return 1;// tab key
+
+            int index = ch[pos] - 'A';
+            if (children[index] == null) return 0;
+            return 1 + children[index].type(ch, pos + 1, wordIndex);
         }
     }
 
@@ -98,62 +167,16 @@ public class SoLong {
         }
 
         // Inserts a word into the trie.
-        public void insert(String word) {
-            HashMap<Character, TrieNode> children = root.children;
-
-            for(int i=0; i<word.length(); i++){
-                char c = word.charAt(i);
-
-                TrieNode t;
-                if(children.containsKey(c)){
-                    t = children.get(c);
-                }else{
-                    t = new TrieNode(c);
-                    children.put(c, t);
-                }
-
-                children = t.children;
-
-                //set leaf node
-                if(i==word.length()-1)
-                    t.isLeaf = true;
-            }
+        public void insert(String word, int wordListIndex) {
+            root.insert(word.toCharArray(), 0, wordListIndex);
         }
 
-        // Returns if the word is in the trie.
-        public boolean search(String word) {
-            TrieNode t = searchNode(word);
-
-            if(t != null && t.isLeaf)
-                return true;
-            else
-                return false;
+        public TrieNode search(String word) {
+            return root.search(word.toCharArray(), 0);
         }
 
-        // Returns if there is any word in the trie
-        // that starts with the given prefix.
-        public boolean startsWith(String prefix) {
-            if(searchNode(prefix) == null)
-                return false;
-            else
-                return true;
-        }
-
-        public TrieNode searchNode(String str){
-            Map<Character, TrieNode> children = root.children;
-            TrieNode t = null;
-            for(int i=0; i<str.length(); i++){
-                char c = str.charAt(i);
-                if(children.containsKey(c)){
-                    t = children.get(c);
-                    children = t.children;
-                }else{
-                    return null;
-                }
-            }
-
-            return t;
+        public int type (String word, int wordListIndex) {
+            return root.type(word.toCharArray(),0,wordListIndex);
         }
     }
-
 }
