5ff0d08ed094e4e7f826e61d2456c234d4ceafa2
cutececil <cecilia.cho@lge.com>
2017-02-20 01:53:32 +0900

    BFS Hanoi

--- algospot/HANOI4B.java
+++ algospot/HANOI4B.java
@@ -10,6 +10,9 @@ import java.util.Queue;
  * Created by cutececil on 2017. 2. 15..
  */
 public class HANOI4B {
+    public static int MAX_DISCS = 12;
+    static int[] discovered = new int[1 << (2 * MAX_DISCS)];
+
     // BFS - precalc
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,31 +21,37 @@ public class HANOI4B {
         // 원반이 12개, 기둥이 4개 - 12 * 12 * 12 * 12 대략 400만 공간이 필요함
         // 불가능한 시간과 메모리이므로 상태 표현을 좀더 압축해야함
         // 미리 원반의 모든 상태의 그래프를 만들어둠
-        precalcBFS(12, 0, 3);
+//        precalcBFS(12, 0, 3);
 
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
-            int N = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine()); // count of disc
 
-            int state = 0;
-            for (int i = 0; i < N-1; ++i) {
+            // 시작 상태
+            int begin = 0;
+            for (int i = 0; i < 4; ++i) { // 기둥
                 String[] AB = br.readLine().split(" ");
-                int A = Integer.parseInt(AB[0]);
+                int A = Integer.parseInt(AB[0]); // 원반 개수
 
-                for (int j = 0; j < A; j++) {
-                    int value = Integer.parseInt(AB[1]); // index기둥에 value원반이 있다고 셋팅
-                    state = set(state, j, value);
+                for (int j = 0; j < A; j++) { // 원반 개수만큼 루프를 돌면서 어떤 원반이 i기둥에 있는지 set
+                    int value = Integer.parseInt(AB[j + 1]);
+                    begin = set(begin, i, value - 1); // i 기둥에 value 원반이 있다고 set
                 }
-
-                System.out.println();
             }
+
+            // 끝 상태 - 모두 옮겨저서 정렬된 상태
+            int end = 0;
+            for (int j = N - 1; j >= 0; --j) { // disc
+                end = set(end, 3, j); // 기둥은 항상 4개이므로 마지막 3번째 기둥에 모든 원반을 세팅
+            }
+
+            System.out.println(precalcBFS(N, begin, end));
         }
+
 
         br.close();
         out.close();
     }
-
-    public static int MAX_DISCS = 12;
 
     static int get(int state, int index) {
         return (state >> (index * 2)) & 3;
@@ -51,8 +60,6 @@ public class HANOI4B {
     static int set(int state, int index, int value) {
         return (state & ~(3 << (index * 2))) | (value << (index * 2));
     }
-
-    static int[] discovered = new int[1 << (MAX_DISCS)];
 
     // discs개의 원반이 있고 각 원반의 시작 위치와 목표 위치가 begin, end에 주어질 때 최소 움직임의 수를 계산한다
     // sortGame과 같은 형태이나 map을 정수(비트)로 처리한 것이 차이.
@@ -86,7 +93,7 @@ public class HANOI4B {
                     if (i != j && (top[j] == -1 || top[j] > top[i])) { // 이동 가능
                         int there = set(here, top[i], j);
                         if (discovered[there] == -1) {
-                            discovered[here] = discovered[there] + 1;
+                            discovered[there] = discovered[here] + 1;
                             if (there == end) return discovered[there];
                             queue.add(there);
                         }
