60519b76af6a80363ca51b5290c613918ec55977
cecilia.cho <cecilia.cho@lge.com>
2017-02-20 15:06:09 +0900

    BFS Hanoi

--- algospot/HANOI4.java
+++ algospot/HANOI4.java
@@ -2,16 +2,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by cutececil on 2017. 2. 15..
  */
-public class HANOI4B {
+public class HANOI4 {
     public static int MAX_DISCS = 12;
-    static int[] discovered = new int[1 << (2 * MAX_DISCS)]; // 2에 2*12
+    static BitSet[] hanoi = new BitSet[MAX_DISCS + 1];
+    static HashMap<BitSet, Integer> discovered = new HashMap<>();
 
     // BFS - precalc
     public static void main(String[] args) throws Exception {
@@ -20,15 +19,23 @@ public class HANOI4B {
 
         // 원반이 12개, 기둥이 4개 - 12 * 12 * 12 * 12 대략 400만 공간이 필요함
         // 불가능한 시간과 메모리이므로 상태 표현을 좀더 압축해야함
-        // 미리 원반의 모든 상태의 그래프를 만들어둠
-        // 0 ~ 3 기둥에 원반은 {1,.. ~ 12} value
-//        precalcBFS(12, 0, 3);
+        // 미리 원반의 모든 상태의 그래프를 만들어두는데 오른쪽 기둥에 정렬된 상태를 루트로 둠 (sortgame과 같은 이치)
+        // 0 ~ 3 기둥에 원반은 {0,.. ~ 11} value
+//        for (int i = 0; i < MAX_DISCS; i++) {
+//            hanoi[i] = new BitSet(MAX_DISCS);
+//        }
+
+        for (int i = 1; i <= MAX_DISCS; i++) {
+            BitSet state = new BitSet(i);
+            state.set(0, i); // 0부터 끝까지 모든 비트를 true셋팅
+            precalcBFS(state, i); // i: 원반 개수
+        }
 
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
             int N = Integer.parseInt(br.readLine()); // count of disc
 
-            // 시작 상태
+            // 입력 상태
             int begin = 0;
             for (int i = 0; i < 4; ++i) { // 기둥 0, 1, 2, 3으로 총 4개
                 String[] AB = br.readLine().split(" ");
@@ -36,17 +43,17 @@ public class HANOI4B {
 
                 for (int j = 0; j < A; j++) { // 원반 개수만큼 루프를 돌면서 어떤 원반이 i기둥에 있는지 set
                     int value = Integer.parseInt(AB[j + 1]);
-                    begin = set(begin, i, value -1); // i 기둥에 value 원반이 있다고 set
+                    begin = set(begin, i, value); // i 기둥에 value 원반이 있다고 set
                 }
             }
 
             // 끝 상태 - 모두 옮겨저서 정렬된 상태
-            int end = 0;
-            for (int j = N-1; j >= 0; --j) { // disc
-                end = set(end, 3, j); // 기둥은 항상 4개이므로 마지막 3번째 기둥에 모든 원반을 세팅
-            }
+//            int end = 0;
+//            for (int j = N - 1; j >= 0; --j) { // disc
+//                end = set(end, 3, j); // 기둥은 항상 4개이므로 마지막 3번째 기둥에 모든 원반을 세팅
+//            }
 
-            System.out.println(precalcBFS(N, begin, end));
+           System.out.println(discovered.get(begin));
         }
 
 
@@ -62,29 +69,25 @@ public class HANOI4B {
         state &= ~(3 << (index * 2));
         state |= value << (index * 2);
         return state;
-//        return (state & ~(3 << (index * 2))) | (value << (index * 2));
     }
 
-    // discs개의 원반이 있고 각 원반의 시작 위치와 목표 위치가 begin, end에 주어질 때 최소 움직임의 수를 계산한다
-    // sortGame과 같은 형태이나 map을 정수(비트)로 처리한 것이 차이.
-    static int precalcBFS(int discs, int begin, int end) {
-        if (begin == end) return 0;
+    // state 현재 상태, N 원반의 개수
+    // sortGame과 같은 형태이나 Bit 처리한 것이 차이로 미리 모든 형태의 그래프를 준비해둠.
+    static void precalcBFS(BitSet state, int N) {
 
-        Queue<Integer> queue = new LinkedList<>();
-        Arrays.fill(discovered, -1);
-
-        // begin root
-        queue.add(begin);
-        discovered[begin] = 0;
+        Queue<BitSet> queue = new LinkedList<>();
+        queue.add(state);
+        discovered.put(state, 0);
 
         while (!queue.isEmpty()) {
-            int here = queue.poll(); // retrieve and remove
+            BitSet here = queue.poll(); // retrieve and remove
+            int cost = discovered.get(here);
 
             // 각 기둥에서 제일 위에 있는 원반의 번호를 계산한다
             int[] top = {-1, -1, -1, -1};
-            for (int i = discs - 1; i >= 0; --i) {
-                int index = get(here, i);
-                top[index] = i;
+            for (int i = N - 1; i >= 0; --i) {
+                int index = here.nextSetBit(i);
+                top[index & 3] = i; // 11번 기둥 위치는? 4개 index중에 있다고 업뎃, 10기둥위치는? 같은 기둥이라면 덮어씀... 가장 작은 기둥으로 top이 다 채워짐
             }
 
             //i번 기둥의 맨 위에 있는 원반을 j번 기둥으로 옮긴다
@@ -95,19 +98,17 @@ public class HANOI4B {
                 for (int j = 0; j < 4; j++) {
                     // j번 기둥은 비어 있거나 맨위의 원반이 더 커야 한다
                     if (i != j && (top[j] == -1 || top[j] > top[i])) { // 이동 가능
-                        int there = set(here, top[i], j);
-                        if (discovered[there] == -1) {
-                            discovered[there] = discovered[here] + 1;
-                            if (there == end) return discovered[there];
-                            queue.add(there);
+                        BitSet there = here;
+                        there.clear(top[i]); // top[i] 원반을 j기둥으로 이동
+                        there.set(j);
+                        if (discovered.containsKey(there) == false) {
+                            discovered.put(there, cost + 1);
                         }
                     }
                 }
             }
-
         }
-
-        return -1;
     }
 
 }
+
