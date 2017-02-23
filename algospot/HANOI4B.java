9d67828483abd0314e5f8aecac9aeb303585911f
cecilia.cho <cecilia.cho@lge.com>
2017-02-23 15:17:21 +0900

    Dijkstra Shortest Path

--- algospot/HANOI4B.java
+++ algospot/HANOI4B.java
@@ -11,65 +11,64 @@ import java.util.Queue;
  */
 public class HANOI4B {
     public static int MAX_DISCS = 12;
+    public static int MAX_TOWER = 4;
     static int[] discovered = new int[1 << (2 * MAX_DISCS)]; // 2에 2*12
 
-    // BFS - precalc
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 
-        // 원반이 12개, 기둥이 4개 - 12 * 12 * 12 * 12 대략 400만 공간이 필요함
+        // 원반이 12개, 기둥이 4개 - 4에 12승으로 4대략 400만? 공간이 필요함
         // 불가능한 시간과 메모리이므로 상태 표현을 좀더 압축해야함
-        // 미리 원반의 모든 상태의 그래프를 만들어둠
-        // 0 ~ 3 기둥에 원반은 {1,.. ~ 12} value
-//        precalcBFS(12, 0, 3);
+        // 미리 원반의 모든 상태의 그래프를 만들어두거나 Bidirectional Search를 해야함
+        // {0,1,2,3} 기둥 4개에 원반은 12개로 {0,.. ~ 11} value (0이 가장 작은 원반
+//        bfsHanoi(12, 0, 3);
 
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
-            int N = Integer.parseInt(br.readLine()); // count of disc
+            int N = Integer.parseInt(br.readLine());            // count of disc
 
-            // 시작 상태
+            // 시작 상태 - 입력
             int begin = 0;
-            for (int i = 0; i < 4; ++i) { // 기둥 0, 1, 2, 3으로 총 4개
+            for (int i = 0; i < MAX_TOWER; ++i) {               // 기둥 {0, 1, 2, 3}으로 총 4개
                 String[] AB = br.readLine().split(" ");
-                int A = Integer.parseInt(AB[0]); // i기둥에 놓인 원반 개수
+                int eachN = Integer.parseInt(AB[0]);            // i기둥에 놓인 원반 개수
 
-                for (int j = 0; j < A; j++) { // 원반 개수만큼 루프를 돌면서 어떤 원반이 i기둥에 있는지 set
-                    int value = Integer.parseInt(AB[j + 1]);
-                    begin = set(begin, value - 1, i); // i 기둥에 value 원반이 있다고 set
+                for (int j = 0; j < eachN; j++) {               // 원반 개수만큼 루프를 돌면서 어떤 원반이 i기둥에 있는지 set
+                    int value = Integer.parseInt(AB[j + 1]);    // value는 원반 번호로 {1~..12} 로 입력됨
+                    begin = set(begin, value - 1, i);     // i 기둥에 value 원반이 있다고 set, 내부적으로 0부터 시작하도록 - 1
                 }
             }
 
-            // 끝 상태 - 모두 옮겨저서 정렬된 상태
+            // 끝 상태 - 모두 옮겨저서 기둥 3번에 정렬된 상태
             int end = 0;
-            for (int j = 0; j < N; j++) { // 원반 개수 N
-                end = set(end, j, 3); // 기둥은 항상 4개이므로 마지막 3번째 기둥에 모든 원반을 세팅
+            for (int j = 0; j < N; j++) {                       // 원반 개수 N
+                end = set(end, j, 3);                     // 기둥은 항상 4개이므로 마지막 3번째 기둥에 모든 원반을 세팅
             }
 
-            System.out.println(precalcBFS(N, begin, end));
+            System.out.println(bfsHanoi(N, begin, end));        // begin 상태에서 완전 정렬된 end까지 옮긴 횟수
         }
-
 
         br.close();
         out.close();
     }
 
-    // disc 원반은 어느 기둥에 있는가?
+    // disc번 원반은 어느 기둥에 있는가?
     static int get(int state, int disc) {
         return (state >> (disc * 2)) & 3;
     }
 
-    // tower 기둥에 disc번 원반이 있다고 state에 해당 비트를 켠다
+    // tower번 기둥에 disc번 원반이 있다고 state에 해당 비트를 켠다
     static int set(int state, int disc, int tower) {
         state &= ~(3 << (disc * 2));
         state |= tower << (disc * 2);
         return state;
-//        return (state & ~(3 << (index * 2))) | (value << (index * 2));
+        // return (state & ~(3 << (index * 2))) | (value << (index * 2));
     }
 
     // discs개의 원반이 있고 각 원반의 시작 위치와 목표 위치가 begin, end에 주어질 때 최소 움직임의 수를 계산한다
     // sortGame과 같은 형태이나 map을 정수(비트)로 처리한 것이 차이.
-    static int precalcBFS(int discs, int begin, int end) {
+    static int bfsHanoi(int discs, int begin, int end) {                // discs 원반 개수
         if (begin == end) return 0;
 
         Queue<Integer> queue = new LinkedList<>();
@@ -80,36 +79,34 @@ public class HANOI4B {
         discovered[begin] = 0;
 
         while (!queue.isEmpty()) {
-            int here = queue.poll(); // retrieve and remove
+            int here = queue.poll();
 
             // 각 기둥에서 제일 위에 있는 원반의 번호를 계산한다
             int[] top = {-1, -1, -1, -1};
-            for (int i = discs - 1; i >= 0; --i) {
-                int index = get(here, i);
-                top[index] = i;
-            }
+            for (int i = discs - 1; i >= 0; --i) {                      // 원반은 {0,~11} 0번이 가장 작은 원반
+                int tower = get(here, i);                               // 11번원반은 어느 기둥에 있나? tower[0~3] = 원반 번호 기록
+                top[tower] = i;                                         // top[0~3]은 10번 기록후 3번으로 계속 덮어써지나,
+            }                                                           // 작은 원반이 위에 있으므로 유효한 로직
 
             //i번 기둥의 맨 위에 있는 원반을 j번 기둥으로 옮긴다
-            for (int i = 0; i < 4; i++) {
-                // i번 기둥에 원반이 없으면 안된다
-                if (top[i] == -1) continue;
+            for (int i = 0; i < MAX_TOWER; i++) {
 
-                for (int j = 0; j < 4; j++) {
+                if (top[i] == -1) continue;                             // i번 기둥에 원반이 없다면 다음 i기둥 진행
+
+                for (int j = 0; j < MAX_TOWER; j++) {
                     // j번 기둥은 비어 있거나 맨위의 원반이 더 커야 한다
-                    if (i != j && (top[j] == -1 || top[j] > top[i])) { // 이동 가능
-                        int there = set(here, top[i], j);
-                        if (discovered[there] == -1) {
-                            discovered[there] = discovered[here] + 1;
-                            if (there == end) return discovered[there];
-                            queue.add(there);
+                    if (i != j && (top[j] == -1 || top[j] > top[i])) {  // j번 기둥에 아무것도 없거나 j번 기둥의 원반이 i보다 크다면 이동가능
+                        int there = set(here, top[i], j);               // j기둥으로 i에 있는 원반번호 top[i]를 옮긴다
+                        if (discovered[there] == -1) {                  // 이런 4개 기둥 상태가 이미 발견되었었나?
+                            discovered[there] = discovered[here] + 1;   // 옮긴 횟수 + 1
+                            if (there == end) return discovered[there]; // end 는 찾고자하는 기둥 상태로 모두 3기둥에 정렬된 상태라면 횟수를 리턴
+                            queue.add(there);                           // 큐에 넣어서 there기둥 상태에서 더 옮겨볼 상태가 있는지 기둥 상태를 더 바꿔보자
                         }
                     }
                 }
             }
-
         }
 
         return -1;
     }
-
 }
--- algospot/ROUTING.java
+++ algospot/ROUTING.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by cutececil on 2017. 2. 20..
@@ -17,13 +14,20 @@ public class ROUTING {
 
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
-            int N = Integer.parseInt(br.readLine());
+            String[] VE = br.readLine().split(" ");
+            int V = Integer.parseInt(VE[0]);
+            int E = Integer.parseInt(VE[1]);
 
-            for (int i = 0; i < N; ++i) {
-                String[] AB = br.readLine().split(" ");
-                int A = Integer.parseInt(AB[0]);
-                int B = Integer.parseInt(AB[1]);
+            EdgeWeightedGraph g = new EdgeWeightedGraph(V);
+            for (int i = 0; i < E; ++i) { // 간선의 개수
+                String[] UVW = br.readLine().split(" ");
+                int u = Integer.parseInt(UVW[0]);
+                int v = Integer.parseInt(UVW[1]);
+                double w = Double.parseDouble(UVW[2]);
+                g.addEdge(u, v, w);
             }
+
+            out.printf("%.10f\n", g.dijkstra(0));             // distance[v-1] has total cost from 0 vertex(root)
         }
 
         br.close();
@@ -32,63 +36,69 @@ public class ROUTING {
 
     static class EdgeWeightedGraph {
         ArrayList<Edge>[] adjList;
-        boolean[] visited;
-        int V;
+        double[] distance;
+        int V; // Vertex Cnt
 
         static class Edge {
-            int from, to;
-            double weight;
+            int v;
+            double w;
 
-            Edge(int from, int to, double weight) {
-                this.from = from;
-                this.to = to;
-                this.weight = weight;
+            Edge(int v, double w) {
+                this.v = v; // to vertex
+                this.w = w; // weight
+            }
+
+            @Override
+            public String toString() {
+                return v + "(" + w + ")";
             }
         }
 
         EdgeWeightedGraph(int V) {
             this.V = V;
-            visited = new boolean[V];
+            distance = new double[V];
+            Arrays.fill(distance, Double.MAX_VALUE);            // cost를 우선 Max로 설정하고 더 작은 cost가 있다면 업데이트 되도록 한다
+
             adjList = (ArrayList<Edge>[]) new ArrayList[V];
-            for (int i = 0; i < V; ++i) {
-                adjList[i] = new ArrayList<>(V);
-            }
+            for (int i = 0; i < V; ++i) adjList[i] = new ArrayList<>(V);
         }
 
-        // priority - 가중치 있는 그래프는 dijkstra, 없다면 일반적인 bfs 사용
-        void dijkstra(int start, int[] distance, int[] parent) {
-            distance = new int[adjList.length];
-            parent = new int[adjList.length];
-            Arrays.fill(distance, -1);
-            Arrays.fill(parent, -1);
+        // 참고 : priority - 가중치(0이상) 있는 그래프는 dijkstra, 없다면 일반적인 bfs 사용
+        // dijkstra는 root에서 모든 정점을 거치면서 최단 거리(cost)를 저정하고 마지막 정점에는 최종 최단거리 값이 저장되므로 dist[v-1]을 리턴함
+        double dijkstra(int start) {
+            PriorityQueue<Edge> pq = new PriorityQueue<>(V, (Edge o1, Edge o2) -> {
+                if (o1.w > o2.w) return 1;
+                else if (o1.w < o2.w) return -1;
+                else return 0;
+            });
 
-            Queue<Integer> pq = new PriorityQueue<>();
-            // root
-            distance[start] = 0;
-            parent[start] = start;
-            pq.add(start);
+            distance[start] = 1.0d;                             // root
+            pq.add(new Edge(start, 1.0d));                  // new Edge - w를 1로 초기화 하는 이유는 가중치가 증폭(multiply)되므로
 
             while (!pq.isEmpty()) {
-                int here = pq.poll(); // retrieve and remove
+                Edge hereE = pq.poll();                         // retrieve and remove
+                int here = hereE.v;
+                double hereCost = hereE.w;
 
-                for (Edge edge : adjList[here]) {
+                if (distance[here] < hereCost) continue;        // here 점까지 온적이 있었는데, 그때 cost가 더 낮았다면 큐에서 꺼내기만하고 패스 다음진행
 
-                    // 처음 발견된 정점이면 큐에 넣는다
-//                    if (distance[there] == -1) {
-//                        distance[there] = distance[here] + 1; // 여기까지 온 거리에 +1
-//                        parent[there] = here;
-//                        pq.add(there);
-//                    }
+                for (Edge thereE : adjList[here]) {
+                    int there = thereE.v;
+                    double cost = thereE.w * hereCost;          // cost를 비교해서 방문을 할지 말지를 결정하는 단계
+                    if (distance[there] > cost) {               // bfs에서는 처음 발견하면 큐에 넣었지만,
+                        distance[there] = cost;                 // dij는 [Here까지의 cost + There 까지 cost]가 기존 there보다 작으면 dist를 갱신하고 pq에 넣는다
+                        pq.add(new Edge(there, cost));          // ** new Edge(v, w) : 기존 입력된 w자체를 바꾸면 안된다. new해서 큐에 넣자!
+                    }
                 }
             }
+
+            return distance[V - 1];                             // distance[V-1]에 0부터 마지막점까지의 cost가 저장되어 있음.
         }
 
-        // undirected
-        public void addEdge(int u, int v, int w) {
-            adjList[u].add(new Edge(u,v,w));
-            adjList[v].add(new Edge(v,u,w));
+        // undirected graph - add Edge
+        public void addEdge(int u, int v, double w) {
+            adjList[u].add(new Edge(v, w));
+            adjList[v].add(new Edge(u, w));
         }
-
     }
-
 }
