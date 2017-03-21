6260a2f0f39034279d6e622ba5c11e02f830a724
cecilia.cho <cecilia.cho@lge.com>
2017-03-21 15:02:03 +0900

    world project

---
+++ team/AProblem01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by cutececil on 2017. 3. 20..
+ */
+public class AProblem01 {
+    public static void main(String[] args) { // class Solution
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            
+        }
+        
+        System.out.println(solve());
+    }
+    
+    static int solve() {
+        int result = 0;
+        return result;
+    }
+}
---
+++ team/AProblem03.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by cutececil on 2017. 3. 20..
+ */
+public class AProblem03 {
+    public static void main(String[] args) { // class Solution
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            
+        }
+        
+        class Sol {
+            int solve() {
+                int result = 0;
+                return result;
+            }
+        }
+        
+        System.out.println(new Sol().solve());
+    }
+    
+}
