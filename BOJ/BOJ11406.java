6617821796c691cb59b32d6ba622d75ae7464f9b
cecilia.cho <cecilia.cho@lge.com>
2017-04-10 10:17:10 +0900

    Maximum Flow

--- BOJ/BOJ11406.java
+++ BOJ/BOJ11406.java
@@ -4,7 +4,7 @@ import java.util.*;
  * Created by cutececil on 2017. 4. 10..
  */
 // Maximum Flow
-public class BOJ11046 {
+public class BOJ11406 {
     public static void main(String[] args) { // class Solution
         Scanner sc = new Scanner(System.in);
         
