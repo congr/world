d784469256445ca1ed1d6c01f630c74bb8a503d2
cecilia.cho <cecilia.cho@lge.com>
2017-05-25 13:24:13 +0900

    2 x n 타일링

---
+++ BOJ-DP/BOJ11727_2NTile2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * Created by cutececil on 2017. 5. 25..
+ */
+// 2×n 직사각형을 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오
+public class BOJ11727_2NTile2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] D = new int[N + 3]; // D[2] = 2를 쓸려면 0이 입력되었을 때 + 3 되어야 함
+
+        D[1] = 1;
+        D[2] = 3;
+        for (int i = 3; i <= N; i++) {
+            D[i] = (2 * D[i - 2] + D[i - 1]) % 10007;
+        }
+
+        System.out.println(D[N]);
+
+        //System.out.println(solve(N));
+    }
+
+    // Bottom-up recursive
+    static int[] D = new int[1001];
+
+    static int solve(int n) {
+        if (n <= 1) return 1;
+        if (n == 2) return 3;
+
+        if (D[n] > 0) // n 이 3보다 크다면 채우는 방법이 반드시 0보다는 큰 값이 존재한다
+            return D[n];
+
+        D[n] = (2 * solve(n - 2) + solve(n - 1)) % 10007;
+        return D[n];
+    }
+}
